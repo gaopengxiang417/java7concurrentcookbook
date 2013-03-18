4f3da617088af45382c975e6fa1f5410cf682d72
gaopengxiang417 <gpx417@hotmail.com>
2013-03-18 10:36:46 +0800

    添加了helloworld程序，测试git是否能够提交

---
+++ src/com/gao/first/HelloWorld.java
@@ -0,0 +1,13 @@
+package com.gao.first;
+
+/**
+ * User: wangchen.gpx
+ * Date: 13-3-18
+ * Time: 上午10:20
+ */
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("hello world");
+
+    }
+}
