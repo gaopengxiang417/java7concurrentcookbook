f67da0576ff52d45b27a0f849373aaee4699c771
gaopengxiang417 <gaopengxiang417@163.com>
2013-09-30 10:58:48 +0800

    创建线程的runnable接口的开发来计算九九乘法表

---
+++ src/com/gao/threadmanagment/MyCalculator.java
@@ -0,0 +1,38 @@
+package com.gao.threadmanagment;
+
+/**
+ * User: wangchen.gpx
+ * Date: 13-9-30
+ * Time: 上午10:43
+ */
+public class MyCalculator {
+    public static void main(String[] args) throws InterruptedException {
+        Thread[] threads = new Thread[10];
+        for (int i = 0; i < 10; i++) {
+            Thread thread = new Thread(new Calculator(i));
+            threads[i] = thread;
+        }
+
+        for (Thread thread : threads) {
+            thread.start();
+            Thread.sleep(1000);
+        }
+    }
+
+
+}
+class Calculator implements Runnable{
+    private int number;
+
+    Calculator(int number) {
+        this.number = number;
+    }
+
+    @Override
+    public void run() {
+        for (int i = 1; i <= number; i++) {
+            System.out.printf("%d * %d = %d,",i , number , i * number);
+        }
+        System.out.println();
+    }
+}
