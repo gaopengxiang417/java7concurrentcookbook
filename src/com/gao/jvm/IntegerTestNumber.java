d3a784b542f8e103bb9f6d084f7fd7d000bf1077
gaopengxiang417 <gaopengxiang417@163.com>
2013-11-28 21:22:44 +0800

    添加了integer的处理

---
+++ src/com/gao/jvm/IntegerTestNumber.java
@@ -0,0 +1,13 @@
+package com.gao.jvm;
+
+/**
+ * User: wangchen.gpx
+ * Date: 13-11-27
+ * Time: 下午1:22
+ */
+public class IntegerTestNumber {
+    public static void main(String[] args) {
+        int x = 01;
+        System.out.println(x);
+    }
+}
